1023e130a2c5df8133652ed3267b9a67b5b9cf4e
abhinavaa01 <agarwalabhinav91@gmail.com>
2023-02-19 12:30:38 +0530

    Some changes

--- src/Components/Head.js
+++ src/Components/Head.js
@@ -14,7 +14,7 @@ function Head() {
                     <h1 className="intro-head col-md-6 col-10 mx-auto text-wrap">
                         Hi, I am Abhinav, a Web developer
                     </h1>
-                    <a className="btn myButton btn-lg mt-5 rounded-5" href="/">Contact</a>
+                    <a className="btn myButton btn-lg mt-3 mt-md-5 rounded-5" href="/">Contact</a>
                 </div>
                 <div className="col-md-6 col-12 my-4">
                     <img src={MyImg} className="rounded-5 my-auto mw-100" alt="Abhinav Agrawal" />
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,12 @@
+import MyImg from "./media/logo.png"
 function Navbar() {
     return (
         <div className="fixed-top">
             {/* Bootstrap Navbar Starts Here */}
             <nav className="navbar navbar-expand-lg bg-light navbar-specs">
                 <div className="container-fluid ms-lg-5 ms-md-4 ms-3 bg-light">
-                    <a className="navbar-brand fw-bold color-text" href="/">Abhinav Agrawal</a>
+                <img src={MyImg} className="rounded-circle my-auto nav-image mx-2" alt="Abhinav Agrawal" />
+                    <a className="navbar-brand fw-bold color-text me-auto" href="/">Abhinav Agrawal</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
